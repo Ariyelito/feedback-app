1c84b41141f9ee6a3fc8043b7090ac332a903873
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-10-09 13:59:36 -0400

    cleanup base app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog Post'
+    const body = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias nihil amet quas omnis voluptatem accusantium rerum expedita tenetur, incidunt porro? Provident veniam neque eaque nulla dolore officia esse voluptates reiciendis!'
+    const comments = [
+        { id: 1, text: 'I like this' },
+        { id: 2, text: 'I don\'t this' },
+        { id: 3, text: 'I hate this' }
+    ]
+
+    const loading = false
+    if (loading) return <h1>true</h1>
+
+    const showComments = true
+
+    const commentBlock =
+        <div className="comments">
+            <h3>Comments ({comments.length})</h3>
+            <ul>
+                {comments.map((comment, index) => (
+                    <li key={index}>{comment.text}</li>
+                ))}
+            </ul>
+        </div>
+
+    return (
+        <div className="container">
+            <h2 className="title">{title.toUpperCase()}</h2>
+            <p>{body}</p>
+            {showComments && commentBlock}
+        </div>
+    )
 }
 
-export default App;
+export default App
