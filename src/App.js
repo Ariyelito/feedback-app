e947cbef9bb9a47bada5441489d1141c5c4d9712
OrlandoAriel <orlandovillanueva11@gmail.com>
2022-11-04 23:06:06 -0400

    Form, Button, Validations

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from "react"
 import Header from "./components/Header"
-// import FeedbackItem from "./components/FeedbackItem"
 import FeedbackData from "./data/FeedbackData"
 import FeedbackList from "./components/FeedbackList"
 import FeedbackStats from "./components/FeedbackStats"
+import FeedbackForm from "./components/FeedbackForm"
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -18,6 +18,7 @@ function App() {
         <>
             <Header />
             <div className="container">
+                <FeedbackForm />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
